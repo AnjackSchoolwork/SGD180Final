fc7fbfe27ff8bd249ce2c4076c1a05a2040df15c
Anjack <gary.reif@gmail.com>
2017-11-29 11:56:19 -0500

    Created basic menu screen that  does nothing but at least it has a moving button!

--- src/menu.js
+++ src/menu.js
@@ -5,15 +5,29 @@ var menu = function () {
 
 menu.prototype = {
 	preload: function () {
-
+		this.game.stage.backgroundColor = "#66B2AF"
 	},
 
 	create: function () {
-		var logo_title = this.game.add.sprite(this.game.world.width / 2, this.game.world.height / 3, "logo_title")
+		var logo_title = this.game.add.sprite(
+			this.game.world.width / 2,
+			this.game.world.height / 3,
+			"logo_title")
 		logo_title.anchor.set(0.5, 0.5)
+
+		var button_play = this.game.add.button(
+			this.game.world.width / 2, (this.game.world.height / 3) * 2, 
+			"button_play", clickButtonPlay, this, 0, 0, 1)
+		button_play.anchor.set(0.5, 0.5)
+
+		
 	},
 
 	update: function () {
 
 	}
+}
+
+function clickButtonPlay() {
+
 }
--- src/startup.js
+++ src/startup.js
@@ -6,6 +6,7 @@ var startup = function () {
 startup.prototype = {
 	preload: function () {
 		this.game.load.image("logo_title", "img/Logo_Title.png", 512, 128)
+		this.game.load.spritesheet("button_play", "img/Button_Play.png", 128, 64)
 	},
 
 	create: function () {
